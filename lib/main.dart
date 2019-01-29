ae69bddb006071c8a1db02c5a01c90508a0c76c3
wuhx <546446588@qq.com>
2019-01-29 16:06:32 +0800

    three

--- lib/main.dart
+++ lib/main.dart
@@ -19,15 +19,17 @@ Future main() async {
 }
 
 void collectLog(String line) {
+  
   //收集日志
 }
 void reportErrorAndLog(FlutterErrorDetails details) {
+
   //上报错误和日志逻辑
 }
 
 FlutterErrorDetails makeDetails(Object obj, StackTrace stack) {
   // 构建错误信息
-  
+
   return null;
 }
 
